7ba1c6398826872b6612341cc8f4e2be7f5a01b5
JOSBALIN <jonasbalin@gmail.com>
2022-09-18 16:28:59 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-root">
+          <div className="block-1">
+            <h1>Jonas Sylvain Jersild Balin</h1>
+          </div>
+          <div className="block-2"></div>
+          <div className="block-3"></div>
+          <div className="block-4"></div>
+
     </div>
   );
 }
