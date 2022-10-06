7ac18a6201b01ebe08bc8cb29ad2cb10f157dc5d
JOSBALIN <jonasbalin@gmail.com>
2022-10-06 16:22:04 +0200

    Testing window dragging and resizing

--- src/components/desktop.js
+++ src/components/desktop.js
@@ -1,11 +1,15 @@
 import '../App.css';
 import * as React from 'react';
 import Taskbar from './taskbar'
+import Dialog from './Dialog';
+import Resizeable from './resizeable';
 
 export default function Desktop(){
 
     return(
     <div className="desktop-background">
+              {/* <Dialog show={true}/> */}
+              <Resizeable window={<Dialog show={true}/>}/>
     <div className="desktop-frontlayer">
         <Taskbar/>
     </div>
