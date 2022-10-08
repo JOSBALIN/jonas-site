4f464b048d80ff703db536df33760140b1682bd0
JOSBALIN <jonasbalin@gmail.com>
2022-10-08 22:32:21 +0200

    Same as before

--- src/components/desktop.js
+++ src/components/desktop.js
@@ -2,47 +2,57 @@ import "../App.css";
 import * as React from "react";
 import Taskbar from "./taskbar";
 import { AppWindow } from "./appWindow";
-import "./Dialog.css";
+import "./appWindow.css";
 import "./desktop.css";
 import { render } from "@testing-library/react";
 import Resizeable from "./resizeable";
+import { useState } from "react";
 
 export default function Desktop() {
+  // List of open apps to control taskbar icons
+  const [openApps, setOpenApps] = useState([]);
 
-  const applications = [
+  // all applications to be displayed on the desktop, initial states
+  const initialAppStates = [
     {
       id: "1",
       title: "My CV",
       icon: "empty",
-      component: <AppWindow open={true}/>,
+      component: <AppWindow open={true} title={"First title!"} />,
       selected: "false",
       open: "false",
+      style: { backgroundColor: "transparent" },
     },
     {
       id: "2",
       title: "Photos",
       icon: "empty",
-      component: <AppWindow open={true}/>,
+      component: <AppWindow open={true} title={"second title!"} contents={<Resizeable/>}/>,
       selected: "false",
       open: "false",
+      style: { backgroundColor: "transparent" },
     },
     {
       id: "3",
       title: "Music",
       icon: "empty",
-      component: <AppWindow open={true}/>,
+      component: <AppWindow open={true} />,
       selected: "false",
       open: "false",
+      style: { backgroundColor: "transparent" },
     },
     {
       id: "4",
       title: "Trivia",
       icon: "empty",
-      component: <AppWindow open={true}/>,
+      component: <AppWindow open={true} />,
       selected: "false",
       open: "false",
+      style: { backgroundColor: "transparent" },
     },
   ];
+
+  const [applications, setApplications] = useState(initialAppStates);
 
   const launchApp = (app) => {
     console.log("launch!");
@@ -50,29 +60,77 @@ export default function Desktop() {
   };
 
   const handleClick = (event, key, app) => {
-    console.log(key);
-    console.log(app)
-    console.log(app[key].component)
-    event.target.className = "selected";
+    console.log(app);
+    updateObjectInArray(app[key])
+
+    console.log(applications);
     if (event.detail === 2) {
       launchApp(app[key].component);
     }
   };
 
+  const addApplication = (obj) => {
+    setApplications((current) => [...current, obj]);
+  };
 
+  // set selected/deselected desktop app icon styles
+  const updateObjectInArray = (selectedApp) => {
+    setApplications((current) =>
+      current.map((app) => {
+        console.log(app);
+        if (app.id == selectedApp.id) {
+          return { ...app, style: { backgroundColor: "lightblue" } };
+        } else return { ...app, style: { backgroundColor: "transparent" } };
+      })
+    );
+  };
+
+    // ✅ Update one or more objects in a state array
+    const deselectApps = () => {
+      setApplications((current) =>
+        current.map((app) => {
+            return { ...app, style: { backgroundColor: "transparent" } };
+        }));
+    };
 
   return (
     <div className="desktop-background">
-      <div className="parent">
+      <button onClick={() => console.log(applications)}>CONSOLE LOG</button>
+      <button
+        onClick={(e) =>
+          addApplication({
+            id: "5",
+            title: "Trivia2",
+            icon: "empty",
+            component: <AppWindow open={true} />,
+            selected: "false",
+            open: "false",
+            style: { backgroundColor: "lightblue" },
+          })
+        }
+      >
+        asdasdsad
+      </button>
+      <div className="parent" onClick={(event) => deselectApps}>
         {applications.map((app, key, appComp) => (
-          <div className={"div"+app.id} onClick={event => handleClick(event, key, appComp)} key={key} app={app.component}>
+          <div
+            className={"div" + app.id}
+            onClick={(event) =>
+              handleClick(event, key, appComp)
+              //updateObjectInArray(app)
+            }
+            key={key}
+            app={app.component}
+            style={app.style}
+          >
             {app.title}
+            {app.selected}
           </div>
         ))}
       </div>
       <div className="desktop-frontlayer">
         {/* <AppWindow /> */}
-        <Taskbar />
+        <Taskbar className="taskbar" />
       </div>
     </div>
   );
