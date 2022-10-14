3a72c4e18e83445723949976f499648cb5d4c703
JOSBALIN <jonasbalin@gmail.com>
2022-10-14 15:34:56 +0200

    added image selection in photoViewer

--- src/components/applications/photoViewer.js
+++ src/components/applications/photoViewer.js
@@ -1,8 +1,10 @@
 import "./photoViewer.css"
 import ellieJonas from "../../images/photos/EllieJonas.jpg";
 import { useState } from "react";
+import { render } from "@testing-library/react";
+import { useEffect } from "react";
 
-export default function PhotoViewer() {
+export default function PhotoViewer(props) {
     const photosMetadata = [
         {
             photo: "EllieJonas.jpg",
@@ -26,34 +28,44 @@ export default function PhotoViewer() {
         },
     ]
 
-
-
-
+    
     // Import photo code borrowed from https://gist.github.com/shaquille-galimba/64f462f0b119945630427f9bedeceba7
     const importAll = (r) => {
         let images = {};
-      r.keys().forEach((item, index) => { images[item.replace('./', '')] = r(item); });
+        r.keys().forEach((item, index) => { images[item.replace('./', '')] = r(item); });
         return images
     }
-
+    
     const images = importAll(require.context('../../images/photos', false, /\.(png|jpe?g|svg)$/));
-
+    
     const photo = (photo, text) => {
-        return (<img src={images[`${photo}`]} alt={text} key={text}/>
+        return (<img src={images[`${photo}`]} alt={text} key={text} onClick={(e) => handleClick(e)} className={"preview-images"}/>
         )
     }
+    
+    const handleClick = (e) => {
+        if(Array.from(document.getElementsByClassName(" selected-photo"))[0] !== undefined){        document.getElementsByClassName(" selected-photo")[0].className = ""}
+        console.log(e.target.src)
 
+        e.currentTarget.className = "selected-photo"
+        console.log(Array.from(document.getElementsByClassName("focused-photo-img"))[0].src)
+        // console.log(Array.from(document.getElementsByClassName(" selected-photo")))
+        Array.from(document.getElementsByClassName("focused-photo-img"))[0].src = e.target.src
+    }
+    
 
 
 
     return(
         <div className="photos-background">
             <div className="focused-photo">
-                {photo("JonasGuitar.jpg", "Guitar W/ fam")}
+                <img className="focused-photo-img">
+
+                </img>
             </div>
             <div className="photos-row">
-            {photosMetadata.map((data) => 
-            <div className="photos-preview">{photo(data.photo, data.text)}
+            {photosMetadata.map((data, index) => 
+            <div className="photos-preview" key={index}>{photo(data.photo, data.text)}
             <p>{data.text}</p>
             </div>
             )}
