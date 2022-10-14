325a795b00dd183c668e0efbf952c05ae9412da6
JOSBALIN <jonasbalin@gmail.com>
2022-10-14 22:42:38 +0200

    Massive refactor of photoViewer, useState implemented

--- src/components/applications/photoViewer.js
+++ src/components/applications/photoViewer.js
@@ -6,52 +6,47 @@ import { useEffect } from "react";
 import topBarImage from "../../images/application-images/photoViewer-top-menu.png";
 
 export default function PhotoViewer(props) {
-    const photosMetadata = [
+    const [selectedPhoto, setSelectedPhoto] = useState("")
+    const [photosMetadata, setPhotosMetadata] = useState([
         {
             photo: "EllieJonas.jpg",
-            text: "My dog and me!"
+            shortText: "My dog and me!"
         },
         {
             photo: "EllieJonas2.jpg",
-            text: "My dog and me (again)!"
+            shortText: "My dog and me (again)!"
         },
         {
             photo: "Jonas.jpg",
-            text: "Master's graduation"
+            shortText: "Master's graduation"
         },
         {
             photo: "JonasGuitar.jpg",
-            text: "Guitar w/ fam"
+            shortText: "Guitar w/ fam"
         },
         {
             photo: "SilasJonas.jpg",
-            text: "W/ colleague at work party"
+            shortText: "W/ colleague at work party"
         },
-    ]
+    ])
 
     
     // Import photo code borrowed from https://gist.github.com/shaquille-galimba/64f462f0b119945630427f9bedeceba7
     const importAll = (r) => {
         let images = {};
-        r.keys().forEach((item, index) => { images[item.replace('./', '')] = r(item); });
+        r.keys().forEach((item) => { images[item.replace('./', '')] = r(item); });
         return images
     }
     
     const images = importAll(require.context('../../images/photos', false, /\.(png|jpe?g|svg)$/));
     
-    const photo = (photo, text) => {
-        return (<img src={images[`${photo}`]} alt={text} key={text} onClick={(e) => handleClick(e)} className={"preview-images"}/>
+    const photoDisplay = (photo, shortText) => {
+        return (<img src={images[`${photo}`]} alt={shortText} key={shortText} onClick={() => handleClick(photo, shortText)} className={"preview-images"}/>
         )
     }
     
-    const handleClick = (e) => {
-        // delete existing selected photo, assign class to new
-        let selectedPhoto = Array.from(document.getElementsByClassName(" selected-photo"))[0]
-        if(selectedPhoto !== undefined) selectedPhoto.className = ""
-
-        e.currentTarget.className = "selected-photo"
-        // Set focused-photo-img element's image to be currentTarget
-        Array.from(document.getElementsByClassName("focused-photo-img"))[0].src = e.target.src
+    const handleClick = (photo, text) => {
+        setSelectedPhoto(images[`${photo}`])
     }
     
 
@@ -61,14 +56,14 @@ export default function PhotoViewer(props) {
         <div className="photos-background">
             <div className={"top-menu-photos"}>      <img className="top-menu-img-photos" src={topBarImage}></img></div>
             <div className="focused-photo">
-                <img className="focused-photo-img" src={images[`${"EllieJonas.jpg"}`]}>
+                <img className="focused-photo-img" src={selectedPhoto}>
 
                 </img>
             </div>
             <div className="photos-row">
             {photosMetadata.map((data, index) => 
-            <div className="photos-preview" key={index}>{photo(data.photo, data.text)}
-            <p>{data.text}</p>
+            <div className="photos-preview" key={index}>{photoDisplay(data.photo, data.shortText)}
+            <p>{data.shortText}</p>
             </div>
             )}
             </div>
