2722fff8d0ea1f865ddefd7eefa8d638729bc66d
JOSBALIN <jonasbalin@gmail.com>
2022-12-06 16:17:43 +0100

    contact form style changes

--- src/components/applications/contact.js
+++ src/components/applications/contact.js
@@ -23,25 +23,27 @@ export default function Contact() {
   };
 
   return (
-      <form onSubmit={sendEmail}>
-        <div className={"top-menu-contact"}>
-          {/* Send button is invisible, overlaid the actual image */}
-          <button
-            type="submit"
-            id="send-button"
-            onMouseEnter={() => setTopImage(topBarImageHover)}
-            onMouseLeave={() => setTopImage(topBarImage)}
-            onMouseUp={() => setTopImage(topBarImageHover)}
-            onMouseDownCapture={() => setTopImage(topBarImagePressed)}
-          ></button>
-          <img className="top-menu-img-contact" src={topImage}></img>
-        </div>
-
-        <div className="top-inputs">
+    <form onSubmit={sendEmail}>
+      <div className={"top-menu-contact"}>
+        {/* Send button is invisible, overlaid the actual image */}
+        <button
+          type="submit"
+          id="send-button"
+          onMouseEnter={() => setTopImage(topBarImageHover)}
+          onMouseLeave={() => setTopImage(topBarImage)}
+          onMouseUp={() => setTopImage(topBarImageHover)}
+          onMouseDownCapture={() => setTopImage(topBarImagePressed)}
+        ></button>
+        <img className="top-menu-img-contact" src={topImage}></img>
+      </div>
+      <div className="top-inputs">
+        <div class="column1-row1"> 
           <div className="labels-icons">
-          <img className="small-mail-icon" src={smallMailIcon} />{" "}
-          <label htmlFor="recipient-mail">To:</label>
+            <img className="small-mail-icon" src={smallMailIcon} />{" "}
+            <label htmlFor="recipient-mail">To:</label>
           </div>
+        </div>
+        <div class="column2-row1"> 
           <input
             type="email"
             id="recipient-mail"
@@ -49,22 +51,42 @@ export default function Contact() {
             readOnly
             disabled
           />
+        
+        </div>
+        <div class="column1-row2"> 
           <div className="labels-icons">
-          <img className="small-mail-icon" src={smallMailIcon} />
-          <label htmlFor="sender-mail">From:</label>
+            <img className="small-mail-icon" src={smallMailIcon} />
+            <label htmlFor="sender-mail">From:</label>
           </div>
-          <input
+        
+        </div>
+        <div class="column2-row2"> 
+        <input
             type="email"
             id="from-mail"
             name="fromMail"
             placeholder="your e-mail here"
           />
-          <div className="labels-icons">
-          <label htmlFor="subject">Subject:</label>
-          </div>
-          <input type="text" id="subject" name="subject" />
         </div>
-        <textarea name="body" id="body"></textarea>
-      </form>
+        <div class="column1-row3"> 
+          <div className="labels-icons">
+            <label htmlFor="subject">Subject:</label>
+          </div>
+        
+        </div>
+        <div class="column2-row3"> 
+        
+        <input type="text" id="subject" name="subject" />
+        </div>
+      </div>
+
+        <div className="labels-column">
+        </div>
+        <div class="inputs-column">
+
+
+        </div>
+      <textarea name="body" id="body"></textarea>
+    </form>
   );
 }
