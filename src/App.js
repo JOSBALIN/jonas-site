3396bc5cadc3cf6d2ebaa7772b8756543ba1aa39
JOSBALIN <jonasbalin@gmail.com>
2022-12-13 18:58:57 +0100

    Testing API calls via netlify

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import './App.css';
 import Desktop from "./components/desktop"
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [todoId, setTodoId] = useState(1);
+  const [todo, setTodo] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  function getNewTodo() {
+    setTodoId((todoId) => (todoId === 20 ? 1 : todoId + 1));
+  }
+
+  useEffect(() => {
+    async function fetchTodo() {
+      const url = `/.netlify/functions/todo?id=${todoId}`;
+      try {
+        setLoading(true);
+        const todo = await fetch(url).then((res) => res.json());
+        setTodo(todo.title);
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchTodo();
+  }, [todoId]);
+
+
   return (
     <div className="app-root">
-      <Desktop/>
+      <div className="App">
+      <p>
+        <button onClick={getNewTodo}> Get another todo </button>
+      </p>
+      <p>{loading ? "Loading..." : todo}</p>
+    </div>
+      {/* <Desktop/> */}
     </div>
   );
 }
