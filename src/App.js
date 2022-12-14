04680ee1d8da5096ccf1554e9c77bba86a0270e7
JOSBALIN <jonasbalin@gmail.com>
2022-12-14 02:17:07 +0100

    further netlify email test

--- src/App.js
+++ src/App.js
@@ -2,9 +2,29 @@ import "./App.css";
 import ContactForm from "./components/ContactForm";
 
 export default function App() {
+  const sendMail = async () => {
+    await fetch(
+      `${process.env.URL}/.netlify/functions/emails/subscribed`,
+      {
+        headers: {
+          "netlify-emails-secret": process.env.NETLIFY_EMAILS_SECRET,
+        },
+        method: "POST",
+        body: JSON.stringify({
+          from: "jonasbalin@gmail.com",
+          to: "jackbamm@hotmail.com",
+          subject: "TEST",
+          parameters: {
+            name: "WIWIWIWIWIWI"
+          },
+        }),
+      }
+    );
+  }
+  
   return (
     <div className="App">
-      <ContactForm />
+      <button onClick={() => sendMail}>AAA</button>
     </div>
   );
 }
