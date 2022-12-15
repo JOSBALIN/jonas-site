627d0733d411c15e462802c5b80f084f3204edf7
JOSBALIN <jonasbalin@gmail.com>
2022-12-15 02:13:57 +0100

    app reverted from mail tests

--- src/App.js
+++ src/App.js
@@ -1,54 +1,27 @@
-import "./App.css";
-import { default as Subscribe } from "./Subscribe.tsx";
+// import "./App.css";
+// import { default as Subscribe } from "./Subscribe.tsx";
 
-export default function App() {
-
-  const sendMail = async () => {
-    await fetch(
-      `http://localhost:8000/.netlify/functions/emails/subscribed`,
-      {
-        headers: {
-          "netlify-emails-secret": process.env.NETLIFY_EMAILS_SECRET,
-        },
-        method: "POST",
-        body: JSON.stringify({
-          from: "jonasbalin@gmail.com",
-          to: "jackbamm@hotmail.com",
-          subject: "THIS IS MESSAGE",
-          parameters: {
-            name: "JOBAL"
-          },
-        }),
-      }
-    );
-  }
+// export default function App() {
   
-  return (
-    <div className="App">
-      <Subscribe></Subscribe>
-      <button onClick={() => console.log(process.env.URL)}>BUTTONTEST</button>
-    </div>
-  );
-}
-
-
-// import './App.css';
-// import Desktop from "./components/desktop"
-// import { useState, useEffect } from 'react';
-
-// function App() {
-
-
-
-  
-
-
 //   return (
-//     <div className="app-root">
-
-//       <Desktop/>
+//     <div className="App">
+//       <Subscribe></Subscribe>
+//       <button onClick={() => console.log(process.env.URL)}>BUTTONTEST</button>
 //     </div>
 //   );
 // }
 
-// export default App;
+
+import './App.css';
+import Desktop from "./components/desktop"
+
+function App() {
+  return (
+    <div className="app-root">
+
+      <Desktop/>
+    </div>
+  );
+}
+
+export default App;
