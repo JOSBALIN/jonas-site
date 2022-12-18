9e4d960b09f3698c847e6497ae46cbc68aefcc5b
JOSBALIN <jonasbalin@gmail.com>
2022-12-18 05:21:16 +0100

    Added PDF changing

--- src/components/applications/portfolio/portfolio.js
+++ src/components/applications/portfolio/portfolio.js
@@ -7,28 +7,52 @@ import { useState } from "react";
 import { AppWindow } from "../../appWindow";
 import PortfolioTextsMenu from "./portfolioTextsMenu";
 import matMul from "./MatrixMultiplication.pdf"
+import infoSecPDF from "./Video Game Information Security and RCE In Source Engine Games.pdf"
 
 export default function Portfolio() {
-  const [displayedDocument, setDisplayedDocument] = useState(<PDFReader document={matMul} selfContained={false} title={"ElemMatMul.pdf"} />);
+  const [displayedDocument, setDisplayedDocument] = useState(
+    <PDFReader
+      document={matMul}
+      selfContained={false}
+      title={"ElemMatMul.pdf"}
+    />
+  );
   const mastersPortfolio = [
+    {
+      title: "InfoSec",
+      document: infoSecPDF,
+      year: "2022",
+    },
     {
       title: "Final Project: Matrix Multiplication",
       document: matMul,
-      year: "2022"
+      year: "2022",
     },
-  ]
+  ];
 
   const summonPDFReader = (document) => {
-    setDisplayedDocument(<PDFReader document={document} selfContained={false} />);
+    setDisplayedDocument(
+      <PDFReader document={document} selfContained={false}/>
+    );
   };
+
+  const selectionPDF = (data) => {
+    setDisplayedDocument(
+
+      <PDFReader document={data.document} title={data.title} year={data.year} selfContained={false}/>
+    )
+  }
 
   return (
     <div className="portfolio-container">
-    <div className="portfolio-top-menu" id="taskbar">      <img className="top-menu-img" src={topBarImage}></img></div>
-    <div className="portfolio-background">
-      <div className="portfolio-left-div">
-        <div className="portfolio-choice">
-          {/* <div className="tab">
+      <div className="portfolio-top-menu" id="taskbar">
+        {" "}
+        <img className="top-menu-img" src={topBarImage}></img>
+      </div>
+      <div className="portfolio-background">
+        <div className="portfolio-left-div">
+          <div className="portfolio-choice">
+            {/* <div className="tab">
             <button
               className="tablinks active"
               onClick={(e) => openTab(e, `Masters`)}
@@ -40,15 +64,24 @@ export default function Portfolio() {
             </button>
           </div> */}
 
-          <PortfolioTextsMenu PDFReader={summonPDFReader} portfolio={mastersPortfolio} title={"Master's"}/>
-          <PortfolioTextsMenu PDFReader={summonPDFReader} portfolio={mastersPortfolio} title={"Bachelor's"}/>
+            <PortfolioTextsMenu
+              PDFReader={summonPDFReader}
+              portfolio={mastersPortfolio}
+              title={"Master's"}
+              selectionPDF={selectionPDF}
+            />
+            <PortfolioTextsMenu
+              PDFReader={summonPDFReader}
+              portfolio={mastersPortfolio}
+              title={"Bachelor's"}
+              selectionPDF={selectionPDF}
+            />
 
-          {/* <PortfolioTextsMenu PDFReader={summonPDFReader} document={infoSecPDF} title={"Bachelor's"}/> */}
-
+            {/* <PortfolioTextsMenu PDFReader={summonPDFReader} document={infoSecPDF} title={"Bachelor's"}/> */}
+          </div>
         </div>
+        <div className="portfolio-right-div">{displayedDocument}</div>
       </div>
-      <div className="portfolio-right-div">{displayedDocument}</div>
-    </div>
     </div>
   );
 }
--- src/components/applications/portfolio/portfolioTextsMenu.js
+++ src/components/applications/portfolio/portfolioTextsMenu.js
@@ -2,35 +2,58 @@ import expandMenu from "../../../images/application-images/portfolio/expand-menu
 import { useState } from "react";
 
 export default function PortfolioTextsMenu(props) {
-    const [contentsHidden, setContentsHidden] = useState(false);
-    const [portfolio, setPortfolio] = useState(props.portfolio)
+  const [contentsHidden, setContentsHidden] = useState(false);
+  const [portfolio, setPortfolio] = useState(props.portfolio);
 
-    function changeHoverState(e) {
-      const title = e.target.firstElementChild;
-      const icon = e.target.lastElementChild;
-      if (e.type == "mouseover") {
-        title.style.filter = "brightness(160%)";
-        icon.style.filter = "brightness(120%)";
-        e.target.style.cursor = "pointer";
-      } else {
-        title.style.display = "";
-        icon.style.display = "";
-        title.style.filter = "brightness(100%)";
-        icon.style.filter = "brightness(100%)";
-      }
+  function changeHoverState(e) {
+    const title = e.target.firstElementChild;
+    const icon = e.target.lastElementChild;
+    if (e.type == "mouseover") {
+      title.style.filter = "brightness(160%)";
+      icon.style.filter = "brightness(120%)";
+      e.target.style.cursor = "pointer";
+    } else {
+      title.style.display = "";
+      icon.style.display = "";
+      title.style.filter = "brightness(100%)";
+      icon.style.filter = "brightness(100%)";
     }
+  }
 
-//(e) => changeHoverState(e)
+  const handleClick = (selectedPDF) => {
+    props.selectionPDF(selectedPDF)
+  }
+
+  //(e) => changeHoverState(e)
   return (
     <div id="Masters" className="tabcontent">
-      <div className="tabcontent-title" onMouseOver={(e) => changeHoverState(e)} onMouseLeave={(e) => changeHoverState(e)} onClick={() => setContentsHidden(!contentsHidden)}>
+      <div
+        className="tabcontent-title"
+        onMouseOver={(e) => changeHoverState(e)}
+        onMouseLeave={(e) => changeHoverState(e)}
+        onClick={() => setContentsHidden(!contentsHidden)}
+      >
         <h2>{props.title}</h2>
-        <img className="menu-control" src={expandMenu} style={contentsHidden ? {transform:"rotate(0deg)"}:{transform:"rotate(180deg)"}}></img>
+        <img
+          className="menu-control"
+          src={expandMenu}
+          style={
+            contentsHidden
+              ? { transform: "rotate(0deg)" }
+              : { transform: "rotate(180deg)" }
+          }
+        ></img>
       </div>
-      <div className="tabcontent-content" id="masters-content" style={contentsHidden ? {display:"none"} : {display:""}}>
+      <div
+        className="tabcontent-content"
+        id="masters-content"
+        style={contentsHidden ? { display: "none" } : { display: "" }}
+      >
         <ul className="portfolio-list">
           {portfolio.map((subject, key) => (
-            <li key={key} onClick={() => props.PDFReader(subject.document)}>{subject.title}</li>
+            <li key={key} onClick={() => handleClick(subject)}>
+              {subject.title}
+            </li>
           ))}
           {/* <li onClick={() => props.PDFReader(props.document)}>
             2022: Master's Thesis - VR Multimedia Analytics Software Usability:
