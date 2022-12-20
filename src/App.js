9987f9b76c4ac63ec175eab474ac7e9d782dc386
JOSBALIN <jonasbalin@gmail.com>
2022-12-20 01:30:26 +0100

    reverted app email test

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import "./App.css";
-import { default as Subscribe } from "./Subscribe.tsx";
+// import "./App.css";
+// import { default as Subscribe } from "./Subscribe.tsx";
 
-export default function App() {
+// export default function App() {
   
+//   return (
+//     <div className="App">
+//       <Subscribe></Subscribe>
+//       <button onClick={() => console.log(process.env.URL)}>BUTTONTEST</button>
+//     </div>
+//   );
+// }
+
+
+import './App.css';
+import Desktop from "./components/desktop"
+
+function App() {
   return (
-    <div className="App">
-      <Subscribe></Subscribe>
-      <button onClick={() => console.log(process.env.URL)}>BUTTONTEST</button>
+    <div className="app-root">
+
+      <Desktop/>
     </div>
   );
 }
 
-
-// import './App.css';
-// import Desktop from "./components/desktop"
-
-// function App() {
-//   return (
-//     <div className="app-root">
-
-//       <Desktop/>
-//     </div>
-//   );
-// // }
-
-// export default App;
+export default App;
