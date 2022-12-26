edc529ed42d9055d0e6c4cfcd3e014bf38a43dfe
JOSBALIN <jonasbalin@gmail.com>
2022-12-26 08:23:44 +0100

    changes to hover in desktop

--- src/components/desktop.js
+++ src/components/desktop.js
@@ -189,7 +189,7 @@ export default function Desktop() {
     };
     
 
-    const [tooltipVisible, cursorPosition, text, element] = UseHoverTooltip();
+  const [tooltipVisible, cursorPosition, text, element] = UseHoverTooltip('This is the hover tooltip');
   const tooltipPosition = {
     position: 'absolute',
     top: cursorY+20,
@@ -198,12 +198,6 @@ export default function Desktop() {
 
   return (
     <div className="desktop-background noselect" id="desktop-background">
-       <div ref={element}>
-        Hover over me (element 1)
-      </div>
-      <div>
-        Hover over me (element 2)
-      </div>
       {tooltipVisible && (
         <div
         className="tooltip"
@@ -221,6 +215,7 @@ export default function Desktop() {
             onClick={(event) => handleClick(event, key, appComp)}
             key={key}
             app={app.component}
+            ref={element}
           >
             <AppIcon title={app.title} icon={app.icon}></AppIcon>
           </div>
