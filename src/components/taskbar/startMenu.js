f55f8aeafbb537f2f588e3725b34663a09d1d3a3
JOSBALIN <jonasbalin@gmail.com>
2022-12-26 19:00:33 +0100

    implemented rudimentary PC shutdown

--- src/components/taskbar/startMenu.js
+++ src/components/taskbar/startMenu.js
@@ -1,12 +1,28 @@
 import "./startMenu.css";
 import OutsideAlerter from "../hooks/OutsideAlerter";
-
+import { useState } from "react";
+import XPShutdownAudio from "./XP-Shutdown.mp3";
+import XPShutDownBackground from "../../images/desktop-shutdown.png"
 
 export default function StartMenu(props) {
+  const [showShutdown, setShowShutdown] = useState(false);
 
-  const  closeSite = () => {
-    if(window.confirm("Are you sure you want to turn this PC off?"))   window.close();
-  }
+  const centeredStyle = {
+    position: 'absolute',
+    top: 200,
+    right: 0,
+    left: 250,
+    margin: 'auto'
+  };
+
+  const shutdownPC = () => {
+    setShowShutdown(true);
+    const audio = new Audio(XPShutdownAudio);
+    audio.addEventListener("ended", () => {
+      window.location.replace('')
+    });
+    audio.play();
+  };
 
   return (
     <OutsideAlerter
@@ -25,7 +41,26 @@ export default function StartMenu(props) {
           <div className="start-menu-button start-menu-app a04"></div>
 
           <div className="start-menu-button log-off"></div>
-          <div className="start-menu-button turn-off-computer" onClick={() => closeSite()}></div>
+          <div
+            className="start-menu-button turn-off-computer"
+            onClick={shutdownPC}
+          ></div>
+
+          {showShutdown && (
+            <div
+              style={{
+                position: "fixed",
+                top: 0,
+                left: 0,
+                zIndex: 200000,
+                width: "100vw",
+                height: "100vw",
+                backgroundColor: "#5a7edc",
+              }}
+            >
+              <img src={XPShutDownBackground} style={centeredStyle}/>
+            </div>
+          )}
         </div>
       }
     ></OutsideAlerter>
