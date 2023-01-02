ecc748d2debb644ae8c654b1fec76ad99f1b8470
JOSBALIN <jonasbalin@gmail.com>
2023-01-02 08:13:00 +0100

    About app shell added

---
+++ src/components/applications/aboutThisApp.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import "./aboutThisApp.css"
+
+const AboutThisApp: React.FC = (props) => {
+   
+    return(
+        <div className="about-this-app-background">
+        </div>
+    )
+}
+
+export default AboutThisApp
