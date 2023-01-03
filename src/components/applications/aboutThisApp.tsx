6cb7395d47de88d95341b664cadce3f6de22e8ba
JOSBALIN <jonasbalin@gmail.com>
2023-01-03 03:55:09 +0100

    About this app initial setup

--- src/components/applications/aboutThisApp.tsx
+++ src/components/applications/aboutThisApp.tsx
@@ -3,10 +3,20 @@ import "./aboutThisApp.css"
 
 const AboutThisApp: React.FC = (props) => {
    
-    return(
-        <div className="about-this-app-background">
+    return (
+      <div className="about-app-background">
+        <div className="about-app-top-menu">
+          <img className="top-menu-img"></img>
         </div>
-    )
+        <div className="about-app-text">
+          <h1 className="about-title">About JonasXP</h1>
+          <h2 className="about-headline">Introduction</h2>
+          <p>
+          This text describes the purpose of the JonasXP site, choices in design, technical implementations, bugs, and room for improvement. Use the menu on the left to jump around to different sections.
+          </p>
+        </div>
+      </div>
+    );
 }
 
 export default AboutThisApp
