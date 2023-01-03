289778ce47eed740dde449c7a83ae0cd0d186d75
JOSBALIN <jonasbalin@gmail.com>
2023-01-03 03:16:42 +0100

    added phone alert for features

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,27 @@
-// import "./App.css";
-// import { default as Subscribe } from "./Subscribe.tsx";
-
-// export default function App() {
-  
-//   return (
-//     <div className="App">
-//       <Subscribe></Subscribe>
-//       <button onClick={() => console.log(process.env.URL)}>BUTTONTEST</button>
-//     </div>
-//   );
-// }
-
-
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Desktop from "./components/desktop"
 
 const App: React.FC = () => {
+  // Detect whether device is phone or PC
+  const [device, setDevice] = useState(null);
+  const [alerted, setAlerted] = useState(false);
+
+  useEffect(() => {
+    const userAgent = navigator.userAgent;
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent)) {
+      setDevice('phone');
+    } else {
+      setDevice('pc');
+    }
+  }, []);
+
+  if(!alerted && device === "phone"){
+    window.alert("It has been detected that you are visiting this page on a phone or tablet.\nsome phone/tablet features are still in development and slight issues may therefore occur\nThank you for your understanding")
+    setAlerted(true)
+  }
+
+
   return (
     <div className="app-root">
       <Desktop/>
